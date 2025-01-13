8b5f90177ea1d2143cadbc5235e4ab3862992195
hlopez49 <hlopez49@cps.edu>
2025-01-13 14:39:54 -0600

    Update 7_indefinite_arguments_args.py

--- 7_indefinite_arguments_args.py
+++ 7_indefinite_arguments_args.py
@@ -1,3 +1,62 @@
+# args = allows you to pass multiple non-key arguments
+# kwargs = allows you to pass multiple keyword-arguments 
+#           unpaking operator
+#           1. positional 2. default 3. keyword 4. ARBITRARY
+
+# Arbitrary and random amount of arguments 
+# def add(a, b):
+#     return a + b
+# print(add(1, 2, 3))
+#  no work cuz too many arguments
+
+# def add(*args):
+#     total = 0
+#     for arg in args:
+#         total += arg
+#     return total
+
+# print(add(1, 2, 3))
+
+# star matters not word in parameter
+
+# def add(*nums):
+#     total = 0
+#     for num in nums:
+#         total += num
+#     return total
+
+# print(add(1, 2, 3))
+
+# def display_name(*args):
+#     for arg in args:
+#         print(arg, end=" ") #puts a space
+    
+# display_name( "Dr.","Spongebob","Harold","Squarepants", "III")
+
+# kwargs
+
+# only prints the actual element 
+
+# def print_address(**kwargs):
+#     for value in kwargs.values():
+#         print(value)
+
+# print_address(street="123 Cookie Rd.", 
+#               city="Chicago", 
+#               state="IL", 
+#               zip="252323")
+
+# does both elements
+
+# def print_address(**kwargs):
+#     for key, value in kwargs.items():
+#         print(f"{key}: {value}")
+
+# print_address(street="123 Cookie Rd.", 
+#               city="Chicago", 
+#               state="IL", 
+#               zip="252323")
+
 # Indefinite Arguments (*args) Practice #1
 # Create a function called sum_squares that takes any number of numeric arguments, and returns the sum of their values squared.
 
